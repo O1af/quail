8e4edec9eb051a57e692dd87a7f11c0371e6c784
Ritij Jutur <45522785+masterspin@users.noreply.github.com>
2024-12-20 21:09:09 -0500

    Added dynamic routing file

---
+++ components/routes.ts
@@ -0,0 +1,10 @@
+import { NextPage } from 'next';
+import Home from '@/app/page';
+import Page from '@/app/app/page';
+
+const Routes = {
+    Home: '/',
+    Page: '/app',
+  };
+  
+export default Routes;
