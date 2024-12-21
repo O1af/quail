f3cfb1eb3ebd27bcbdf7b0ee77534780e45934fd
olafpd <olafpd@umich.edu>
2024-12-21 17:51:31 -0500

    dark mode ftw

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,12 +1,15 @@
 import { SidebarTrigger } from "@/components/ui/sidebar";
 import { ResizableDisplay } from "@/components/ResizableDisplay";
-
+import { ModeToggle } from "@/components/mode-toggle";
 export default function Page() {
   return (
     <>
       <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
         <div className="flex items-center gap-2 px-4">
           <SidebarTrigger className="-ml-1" />
+        </div>
+        <div className="flex items-center gap-2 px-4 ml-auto">
+          <ModeToggle />
         </div>
       </header>
       <ResizableDisplay />
