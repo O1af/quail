b524cc2aab72072227ecb4dbe573c7ac3617db80
olafpd <olafpd@umich.edu>
2024-12-21 16:21:44 -0500

    added react scan and fixed bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* <script src="https://unpkg.com/react-scan/dist/auto.global.js" async /> */}
+        {/* rest of your scripts go under */}
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
