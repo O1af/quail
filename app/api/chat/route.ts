775db5a480aaa42b769b7fbf01d70e175efacb23
Ritij Jutur <45522785+masterspin@users.noreply.github.com>
2024-12-28 13:29:20 -0500

    Added api for gpt-4o-mini

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,5 +1,10 @@
-import { openai } from "@ai-sdk/openai";
 import { streamText } from "ai";
+import { createAzure } from '@ai-sdk/azure';
+
+const azure = createAzure({
+  resourceName: process.env.AZURE_RESOURCE_NAME, // Azure resource name
+  apiKey: process.env.AZURE_API_KEY, // Azure API key
+});
 
 // Allow streaming responses up to 30 seconds
 export const maxDuration = 30;
@@ -8,7 +13,7 @@ export async function POST(req: Request) {
   const { messages } = await req.json();
 
   const result = streamText({
-    model: openai("gpt-4o"),
+    model: azure("gpt-4o-mini"),
     messages,
   });
 
