f1ae49ffdf675fd1acc9727a5a8f7aea7343f7f4
Ritij Jutur <45522785+masterspin@users.noreply.github.com>
2024-12-29 19:01:37 -0500

    Verification fix

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,19 +1,25 @@
 import React from "react";
 import { SignUpForm } from "./components/SignUpForm";
 import Routes from "@/components/routes";
-import { GalleryVerticalEnd } from "lucide-react";
+import Image from "next/image";
 
 const SignUpPage = () => {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
+    <div className="flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-6">
         <a
           href={Routes.Home}
           className="flex items-center gap-2 self-center font-medium"
         >
-          <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
-            {/*TODO: Replace with Logo*/}
-            <GalleryVerticalEnd className="size-4" />
+          <div className="flex h-8 w-8 items-center justify-center rounded-md bg-primary text-primary-foreground">
+            <div className="relative h-full w-full">
+              <Image
+                src="/BotIcon.png"
+                fill
+                className="object-contain"
+                alt="Avatar"
+              />
+            </div>
           </div>
           Quail
         </a>
