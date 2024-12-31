0fa9a596f52ba05bf74d7a4a543a280ba6550dc4
Ritij Jutur <45522785+masterspin@users.noreply.github.com>
2024-12-31 14:48:33 -0500

    Check for user before running runPostgres Endpoing | Can't use JWT due to being server-sided

--- utils/actions/runSQL.ts
+++ utils/actions/runSQL.ts
@@ -1,11 +1,19 @@
 "use server";
 import { PostgresRequest, PostgresResponse } from "@/utils/types/DatabaseTypes";
+import { createClient } from "../supabase/server";
 
 export async function runPostgres(
-  request: PostgresRequest
+  request: PostgresRequest,
 ): Promise<PostgresResponse> {
   if (!process.env.AZURE_FUNCTION_ENDPOINT) {
     throw new Error("Function endpoint not configured");
+  }
+  const supabase = createClient();
+
+  const { data: user, error } = await supabase.auth.getUser();
+
+  if (error || !user) {
+    throw new Error("Unauthorized");
   }
 
   const response = await fetch(
@@ -16,7 +24,7 @@ export async function runPostgres(
         "Content-Type": "application/json",
       },
       body: JSON.stringify(request),
-    }
+    },
   );
 
   if (!response.ok) {
