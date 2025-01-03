8b5fbc2cca19f963a1fb432e4e416779178abc07
olafpd <olafpd@umich.edu>
2025-01-02 19:31:09 -0500

    refactor: simplify runPostgres function by removing session handling

--- utils/actions/runSQL.ts
+++ utils/actions/runSQL.ts
@@ -3,7 +3,7 @@ import { PostgresRequest, PostgresResponse } from "@/utils/types/DatabaseTypes";
 import { createClient } from "../supabase/server";
 
 export async function runPostgres(
-  request: PostgresRequest,
+  request: PostgresRequest
 ): Promise<PostgresResponse> {
   if (!process.env.AZURE_FUNCTION_ENDPOINT) {
     throw new Error("Function endpoint not configured");
@@ -16,27 +16,12 @@ export async function runPostgres(
     throw new Error("Unauthorized");
   }
 
-  const {
-    data: { session },
-    error: sessionError,
-  } = await supabase.auth.getSession();
-
-  if (sessionError || !session) {
-    throw new Error("Unable to retrieve session");
-  }
-
-  const jwt = session.access_token;
-
   const response = await fetch(
     process.env.AZURE_FUNCTION_ENDPOINT + "/runPostgres",
     {
       method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${jwt}`,
-      },
       body: JSON.stringify(request),
-    },
+    }
   );
 
   if (!response.ok) {
