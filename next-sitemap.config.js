a7cb8d85535a64ad22ac7c180d4957912903f684
olafpd <olafpd@umich.edu>
2025-01-29 20:05:43 -0500

    fix: set default siteUrl in sitemap configuration

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.NEXT_PUBLIC_BASE_URL,
+  siteUrl: process.env.NEXT_PUBLIC_BASE_URL || "https://quailbi.com",
   generateRobotsTxt: true,
   sitemapSize: 5000,
   generateIndexSitemap: true,
