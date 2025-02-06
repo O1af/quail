a239a25c4dc6b46bb6c69baee6437681f317760d
olafpd <olafpd@umich.edu>
2025-02-06 12:58:51 -0500

    feat: add DevSidebar component for user authentication and navigation

---
+++ components/sidebar/bi-sidebar.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import * as React from "react";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { createClient } from "@/utils/supabase/client";
+
+import { NavUser } from "@/components/sidebar/nav-user";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarRail,
+} from "@/components/ui/sidebar";
+
+export function DevSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  const [user, setUser] = useState<any>(null);
+  const supabase = createClient();
+  const router = useRouter();
+  useEffect(() => {
+    const fetchUser = async () => {
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
+      setUser(user);
+    };
+    fetchUser();
+  }, [router, supabase]);
+
+  const fullName = user?.user_metadata?.full_name ?? "";
+  const email = user?.user_metadata?.email ?? "";
+  const avatar_url = user?.user_metadata?.avatar_url ?? "";
+
+  const supabaseData = {
+    user: {
+      name: fullName,
+      email: email,
+      avatar: avatar_url,
+    },
+  };
+
+  return (
+    <Sidebar collapsible="icon" {...props}>
+      <SidebarHeader></SidebarHeader>
+      <SidebarContent></SidebarContent>
+      <SidebarFooter>
+        <NavUser user={supabaseData.user} />
+      </SidebarFooter>
+      <SidebarRail />
+    </Sidebar>
+  );
+}
