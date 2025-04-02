f2884c1d4d6e1626c3cd8e887d5ac7ae800da731
Ritij Jutur <45522785+masterspin@users.noreply.github.com>
2025-04-02 10:57:11 -0400

    Colors work now for some reason

--- app/app/(bi)/notes/components/extensions.ts
+++ app/app/(bi)/notes/components/extensions.ts
@@ -16,6 +16,8 @@ import { UploadImagesPlugin } from "novel";
 import { HighlightExtension } from "novel";
 
 import { cx } from "class-variance-authority";
+import GlobalDragHandle from "tiptap-extension-global-drag-handle";
+import AutoJoiner from "tiptap-extension-auto-joiner"; // optional
 
 const aiHighlight = AIHighlight;
 const placeholder = Placeholder;
@@ -118,6 +120,18 @@ const highlightExtension = HighlightExtension.configure({
 
 export const defaultExtensions = [
   starterKit,
+  GlobalDragHandle.configure({
+    dragHandleWidth: 20, // default
+
+    // The scrollTreshold specifies how close the user must drag an element to the edge of the lower/upper screen for automatic
+    // scrolling to take place. For example, scrollTreshold = 100 means that scrolling starts automatically when the user drags an
+    // element to a position that is max. 99px away from the edge of the screen
+    // You can set this to 0 to prevent auto scrolling caused by this extension
+    scrollTreshold: 100, // default
+  }),
+  AutoJoiner.configure({
+    elementsToJoin: ["bulletList", "orderedList"], // default
+  }),
   placeholder,
   tiptapLink,
   tiptapImage,
