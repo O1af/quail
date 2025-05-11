a42d0552a8c0f545fcfb50f5c279b956b36e9ad9
olafpd <olafpd@umich.edu>
2025-05-10 23:52:54 -0400

    chore: remove unused Azure Static Web Apps workflow and related configurations

--- middleware.ts
+++ middleware.ts
@@ -33,6 +33,7 @@ export async function middleware(request: NextRequest) {
   // // Default session handling for other routes
   return await updateSession(request);
 }
+
 export const config = {
   matcher: [
     /*
@@ -40,9 +41,8 @@ export const config = {
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
-     * - .swa (Azure Static Web Apps health check)
      * Feel free to modify this pattern to include more paths.
      */
-    "/((?!_next/static|_next/image|favicon.ico|.swa|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 };
