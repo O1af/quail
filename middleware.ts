d22ad775400861c65bce6a3a5843aa90f0560740
olafpd <olafpd@umich.edu>
2025-05-10 23:36:22 -0400

    feat: enhance Azure workflows and add static web app configuration

--- middleware.ts
+++ middleware.ts
@@ -40,8 +40,9 @@ export const config = {
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
+     * - .swa (Azure Static Web Apps health check)
      * Feel free to modify this pattern to include more paths.
      */
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    "/((?!_next/static|_next/image|favicon.ico|.swa|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 };
